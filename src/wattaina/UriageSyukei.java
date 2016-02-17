68528f8635cab7f1ab8eeaa8e3bf968323469bf8
Kazuki Itou <k_ito@plusize>
2016-02-17 14:30:07 +0900

    完成

--- src/wattaina/UriageSyukei.java
+++ src/wattaina/UriageSyukei.java
@@ -7,199 +7,206 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 
 public class UriageSyukei {
 	public static void main(String[] args) {
-			if(args.length != 1){
-				System.out.println("正しくディレクトリを指定してください。");
-				System.exit(1);
-			}
-			LinkedHashMap<String, String> branchMap = new LinkedHashMap<String, String>();
-			ArrayList<String> branchCode = new ArrayList<String>();
-			//商品定義ファイルを読み込む
-			try{
-				BufferedReader br = new BufferedReader(new FileReader(args[0]+"\\branch.lst"));
-
-				String str;
-				while((str = br.readLine()) != null){
-					String[] branch = str.split(",");
-					if(!(branch[0].matches("^\\d{3}"))){
-						System.out.println("支店定義ファイルのフォーマットが不正です");
-						return;
-					}
-					if(!(branch[1].matches("^\\D{2,3}支店"))){
-						System.out.println("支店定義ファイルのフォーマットが不正です");
-						return;
-					}
-
-					branchMap.put(branch[0], branch[1]);
-					branchCode.add(branch[0]);
-
+		if(args.length != 1){
+			System.out.println("正しくディレクトリを指定してください。");
+			System.exit(1);
+		}
+		LinkedHashMap<String, String> branchMap = new LinkedHashMap<String, String>();
+		ArrayList<String> branchCode = new ArrayList<String>();
+//		商品定義ファイルを読み込む		//
+		try{
+			BufferedReader br = new BufferedReader(new FileReader(args[0]+"\\branch.lst"));
+			String str;
+			while((str = br.readLine()) != null){
+				String[] branch = str.split(",");
+				if(!(branch[0].matches("^\\d{3}"))){
+					System.out.println("支店定義ファイルのフォーマットが不正です");
+					return;
 				}
-				br.close();
-			}catch(IOException e){
-				System.out.println("支店定義ファイルが存在しません");
-			}
-			//商品定義ファイルのフォーマットエラー処理//
-
-			//商品定義ファイルを読み込む
-			LinkedHashMap<String, String> commodityMap = new LinkedHashMap<String, String>();
-			ArrayList<String> commodityCode = new ArrayList<String>();
-			try{
-				BufferedReader br = new BufferedReader(new FileReader(args[0]+"\\commodity.lst"));
-
-				String str;
-				while((str = br.readLine()) != null){
-					String[] commodity = str.split(",");
-					if(!(commodity[0].matches("^\\D{3}\\d{5}"))){
-						System.out.println("商品定義ファイルのフォーマットが不正です");
-						return;
-					}
-					if((commodity[1].matches(",\\n"))){
-						System.out.println("商品定義ファイルのフォーマットが不正です");
-						return;
-					}
-					commodityMap.put(str.substring(0,8), str.substring(9));
-					commodityCode.add(str.substring(0,8));
+				if(!(branch[1].matches("^\\D{2,3}支店"))){
+					System.out.println("支店定義ファイルのフォーマットが不正です");
+					return;
 				}
-				br.close();
-			}catch(IOException e){
-				System.out.println("商品定義ファイルが存在しません");
+				branchMap.put(branch[0], branch[1]);
+				branchCode.add(branch[0]);
 			}
+			br.close();
+		}catch(IOException e){
+			System.out.println("支店定義ファイルが存在しません");
+		}
+//		商品定義ファイルを読み込む		//
+		LinkedHashMap<String, String> commodityMap = new LinkedHashMap<String, String>();
+		ArrayList<String> commodityCode = new ArrayList<String>();
+		try{
+			BufferedReader br = new BufferedReader(new FileReader(args[0]+"\\commodity.lst"));
+			String str;
+			while((str = br.readLine()) != null){
+				String[] commodity = str.split(",");
+				if(!(commodity[0].matches("^\\D{3}\\d{5}"))){
+					System.out.println("商品定義ファイルのフォーマットが不正です");
+					return;
+				}
+				if((commodity[1].matches(",\\n"))){
+					System.out.println("商品定義ファイルのフォーマットが不正です");
+					return;
+				}
+				commodityMap.put(commodity[0], commodity[1]);
+				commodityCode.add(commodity[0]);
+			}
+			br.close();
+		}catch(IOException e){
+			System.out.println("商品定義ファイルが存在しません");
+		}
 
-			ArrayList<String> rcdin = new ArrayList<String>();
-			ArrayList<String> rcdfile = new ArrayList<String>();
-			try{
-				File dir = new File(args[0]);
-			    File[] files = dir.listFiles();
-			    for (int i = 0; i < files.length; i++) {
-			        File file = files[i];
-
-					if(file.getName().matches("^\\d{8}.rcd$")) {
-						rcdfile.add(file.getName());
-
-					}
+		ArrayList<String> rcdin = new ArrayList<String>();
+		ArrayList<String> rcdfile = new ArrayList<String>();
+		LinkedHashMap<String,Integer> rcdMap = new LinkedHashMap<String,Integer>();
+		try{
+			File dir = new File(args[0]);
+			File[] files = dir.listFiles();
+			for (int i = 0; i < files.length; i++) {
+				File file = files[i];
+				if(file.getName().matches("^\\d{8}.rcd$")) {
+					rcdfile.add(file.getName());
+				}
+			}
+			String[] rcd = new String[rcdfile.size()];
+		    int[] rcd2 = new int[rcdfile.size()];
+		    for(int i=0; i < rcdfile.size(); i++){
+		    	rcd = rcdfile.get(i).split("\\.");
+		    	Arrays.sort(rcd);
+			    rcd2[i] = Integer.parseInt(rcd[0]);
+			    if(i+1 != rcd2[i]){
+			    	System.out.println("売上ファイル名が連番になっていません");
+			    	return;
 			    }
-
-			    String[] rcd = new String[rcdfile.size()];
-		    	int[] rcd2 = new int[rcdfile.size()];
-
-		    	for(int i=0; i < rcdfile.size(); i++){
-			    	rcd = rcdfile.get(i).split("\\.");
-			    	Arrays.sort(rcd);
-			    	rcd2[i] = Integer.parseInt(rcd[0]);
-			    	if(i+1 != rcd2[i]){
-			    		System.out.println("売上ファイル名が連番になっていません");
-			    		return;
-			    	}
-			    }
-
-				for(int i = 0; i < rcdfile.size(); i++){
-					BufferedReader br = new BufferedReader(new FileReader(args[0] +"\\" +rcdfile.get(i)));
-					String str;
-					int a = 0;
-					int b = 0;
-					while((str = br.readLine()) != null){
+		    }
+		    for(int i = 0; i < rcdfile.size(); i++){
+				BufferedReader br = new BufferedReader(new FileReader(args[0] +"\\" +rcdfile.get(i)));
+				String str;
+				int a = 0;
+				int b = 0;
+				int[] price = new int[rcdfile.size()];
+				long[] bigprice = new long[rcdfile.size()];
+				while((str = br.readLine()) != null){
+					if((branchMap.get(str) != null)){
 						rcdin.add(str);
-						a++;
-						if(a % 3 == 0){
-							if(!(b == (i / 3))){
-								System.out.println(rcdfile.get(i) + "フォーマットが不正です");
+						while((str = br.readLine()) != null){
+							if((commodityMap.get(str) != null)){
+								rcdin.add(str);
+								while((str = br.readLine()) != null){
+									if(!(str.length() >= 10)){
+										price[i] = Integer.parseInt(str);
+									}else{
+										bigprice[i] = Long.parseLong(str);
+									}
+//									rcdMap.put(rcdin.get(i*2),price[i]);
+									if(rcdMap.get(rcdin.get(i*2)) == null){
+										rcdMap.put(rcdin.get(i*2),price[i]);
+									}else{
+										rcdMap.put(rcdin.get(i*2),rcdMap.get(rcdin.get(i*2)) + price[i]);
+									}
+									if(rcdMap.get(rcdin.get(i*2+1)) == null){
+										rcdMap.put(rcdin.get(i*2+1),price[i]);
+									}else{
+										rcdMap.put(rcdin.get(i*2+1),rcdMap.get(rcdin.get(i*2+1)) + price[i]);
+									}
+									if(rcdMap.get(rcdin.get(i*2)) >= 1000000000 || rcdMap.get(rcdin.get(i*2+1)) >= 1000000000 || bigprice[i] >= 1000000000){
+										System.out.println("合計金額が10桁を超えました");
+										return;
+									}
+								}
+							}else{
+								System.out.println(rcdfile.get(i)+"の支店コードが不正です");
 								return;
 							}
-							b++;
-						}
-					}
-					br.close();
-				}
-			}catch(IOException e){
-				System.out.println("売上ファイルが存在しません");
-			}
-			//売上ファイルファイルフォーマット//
-			for(int i=0; i < (rcdin.size()/3); i++){
-				if(!(rcdin.get(i*3).matches("\\d{3}"))){
-					System.out.println(rcdfile.get(i)+"の支店コードが不正です");
-					return;
-				}
-				if(!(rcdin.get(i*3+1).matches("\\D{3}\\d{5}"))){
-					System.out.println(rcdfile.get(i)+"の商品コードが不正です");
-					return;
-				}
-			}
-			//売上集計//
-			int[] val = new int[rcdin.size()/3];
-			for(int i=0; i < (branchMap.size()/2); i++){
-				for(int j = 0; j <(branchMap.size()/2);j++){
-					if( ! (branchMap.get(rcdin.get(i*3)) == null)){
-						val[i] = Integer.parseInt(rcdin.get(i*3+2));
-						if(branchMap.get(rcdin.get(i*3))==rcdin.get(j*3)){
-							val[i] = val[i]+val[j];
 						}
 					}else{
-						val[i] = 0;
+						System.out.println(rcdfile.get(i)+"の支店コードが不正です");
+						return;
 					}
-				}
-				if(val[i] >= 100000000){
-					System.out.println("合計結果が10桁を超えました");
-					return;
-				}
-			}
-
-			try {
-				File file = new File(args[0]+"\\branch.out");
-				FileWriter fw = new FileWriter(file);
-				String[] branchOut = new String[branchMap.size()];
-				int a=0;
-				for(int i = 0; i < (branchMap.size()); i++){
-					for(int j = 0; j < (rcdin.size()/3); j++){
-						if((branchCode.get(i).equals(rcdin.get(j*3)))){
-							if((rcdin.get(j*3).equals(branchCode.get(i)))){
-								branchOut[i] = (rcdin.get(a*3)+","+branchMap.get(rcdin.get(a*3))+","+val[a]+"\n");
-								a++;
-								break;
-							}
-						}else if(!(branchCode.get(i).equals(rcdin.get(j*3)))){
-							if(!(branchCode.get(i).equals(rcdin.get(j*3)))){
-								branchOut[i] = (branchCode.get(i)+","+branchMap.get(branchCode.get(i))+","+0+"\n");
-							}
+					if(a % 3 == 0){
+						if(!(b == (a / 3))){
+							System.out.println(rcdfile.get(i) + "フォーマットが不正です");
+							return;
 						}
+						b++;
 					}
-					fw.write(branchOut[i]);
+					a++;
 				}
-
-				fw.close();
-			} catch (IOException e) {
-				System.out.println("支店別集計ファイルが存在しません");
+				br.close();
 			}
-			try {
-				File file = new File(args[0]+"\\commodity.out");
-				FileWriter fw = new FileWriter(file);
-				String[] commodityOut = new String[commodityMap.size()];
-
-				int a=0;
-				for(int i = 0; i < (commodityMap.size()); i++){
-					for(int j = 0; j < (rcdin.size()/3); j++){
-						if((commodityCode.get(i).equals(rcdin.get(j*3+1)))){
-							if((rcdin.get(j*3+1).equals(commodityCode.get(i)))){
-								commodityOut[i] = (rcdin.get(a*3+1)+","+commodityMap.get(rcdin.get(a*3+1))+","+val[a]+"\n");
-								a++;
-								break;
-							}
-						}else if(!(commodityCode.get(i).equals(rcdin.get(j*3+1)))){
-							if(!(commodityCode.get(i).equals(rcdin.get(j*3+1)))){
-								commodityOut[i] = (commodityCode.get(i)+","+commodityMap.get(commodityCode.get(i))+","+0+"\n");
-							}
-						}
-					}
-					fw.write(commodityOut[i]);
+		}catch(IOException e){
+			System.out.println("売上ファイルが存在しません");
+		}
+		try {
+			File file = new File(args[0]+"\\branch.out");
+			FileWriter fw = new FileWriter(file);
+			ArrayList branchList = new ArrayList();
+			HashMap branch = new HashMap();
+			int a = 0;
+			for(int i = 0; i < (branchMap.size()); i++){
+				if(rcdMap.get(branchCode.get(i))!=null){
+					branchList.add(rcdMap.get(branchCode.get(i)));
+					branch.put(rcdMap.get(branchCode.get(i)),branchCode.get(i)+','+branchMap.get(branchCode.get(i)));
+				}else{
+					branchList.add(0);
+					branch.put(a,branchCode.get(i)+','+branchMap.get(branchCode.get(i)));
+					a++;
 				}
-
-				fw.close();
-			} catch (IOException e) {
-				System.out.println("商品別集計ファイルが存在しません");
 			}
+			Collections.sort(branchList);
+		    Collections.reverse(branchList);
+			a=0;
+			for(int i = 0; i < (branchMap.size()); i++){
+				if(!(branchList.get(i).equals(0))){
+					System.out.print(branch.get(branchList.get(i)) + "," + branchList.get(i) + "\n");
+				}else{
+					System.out.print(branch.get(a) + "," + branchList.get(i) + "\n");
+					a++;
+				}
+			}
+			fw.close();
+		} catch (IOException e) {
+			System.out.println("支店別集計ファイルが存在しません");
+		}
 
+		try {
+			File file = new File(args[0]+"\\commodity.out");
+			FileWriter fw = new FileWriter(file);
+			ArrayList commodityList = new ArrayList();
+			HashMap commodity = new HashMap();
+			int a = 0;
+			for(int i = 0; i < (commodityMap.size()); i++){
+				if(rcdMap.get(commodityCode.get(i))!=null){
+					commodityList.add(rcdMap.get(commodityCode.get(i)));
+					commodity.put(rcdMap.get(commodityCode.get(i)),commodityCode.get(i)+','+commodityMap.get(commodityCode.get(i)));
+				}else{
+					commodityList.add(0);
+					commodity.put(a,commodityCode.get(i)+','+commodityMap.get(commodityCode.get(i)));
+					a++;
+				}
+			}
+			Collections.sort(commodityList);
+		    Collections.reverse(commodityList);
+			a=0;
+			for(int i = 0; i < (commodityMap.size()); i++){
+				if(!(commodityList.get(i).equals(0))){
+					System.out.print(commodity.get(commodityList.get(i)) + "," + commodityList.get(i) + "\n");
+				}else{
+					System.out.print(commodity.get(a) + "," + commodityList.get(i) + "\n");
+					a++;
+				}
+			}
+			fw.close();
+		} catch (IOException e) {
+			System.out.println("商品別集計ファイルが存在しません");
+		}
 	}
-
 }
