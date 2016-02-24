ce54d2f7d715304b7d733003f2f11ef25f225e6c
Kazuki Itou <k_ito@plusize>
2016-02-24 15:09:38 +0900

    売上ファイルエラー修正

--- src/wattaina/UriageSyukei.java
+++ src/wattaina/UriageSyukei.java
@@ -25,12 +25,11 @@ public class UriageSyukei {
 
 	static String loadFile(String[] args, String files, String regex,
 			LinkedHashMap<String, String> definitionMap,
-			LinkedHashMap<String, Integer> salesMap) throws IOException, FileNotFoundException {
+			LinkedHashMap<String, Long> salesMap) throws IOException, FileNotFoundException {
 		BufferedReader br = null;
 		try{
 			br = new BufferedReader(new FileReader(args[0] + File.separator + files));
 			String str;
-
 			while((str = br.readLine()) != null){
 				String[] format = str.split(",");
 				if(format.length != 2){
@@ -40,7 +39,7 @@ public class UriageSyukei {
 					return "notMatch";
 				}
 				definitionMap.put(format[0],format[1]);
-				salesMap.put(format[0], 0);
+				salesMap.put(format[0], (long) 0);
 			}
 		}catch(FileNotFoundException e){
 			throw e;
@@ -63,58 +62,52 @@ public class UriageSyukei {
 			}
 		}
 		String[] rcd = new String[rcdfile.size()];
-		int[] rcd2 = new int[rcdfile.size()];
 		for(int i=0; i < rcdfile.size(); i++){
 			rcd = rcdfile.get(i).split("\\.");
 			Arrays.sort(rcd);
-			rcd2[i] = Integer.parseInt(rcd[0]);
-			if(i + 1 != rcd2[i]){
+			if(i + 1 != Integer.parseInt(rcd[0])){
 				return false;
 			}
 		}
 		return true;
 	}
 
-	static String aggregateData(String[] args, LinkedHashMap<String, Integer> beanchSalesMap, LinkedHashMap<String, Integer> commoditySalesMap,
-			LinkedHashMap<String, Integer>rcdMap, ArrayList<String> rcdfile) throws IOException{
+	static String aggregateData(String[] args, LinkedHashMap<String, Long> branchSalesMap,
+			LinkedHashMap<String, Long> commoditySalesMap,
+			LinkedHashMap<String, Integer>rcdMap,
+			ArrayList<String> rcdfile) throws IOException{
 		BufferedReader br = null;
 		try{
 		    for(int i = 0; i < rcdfile.size(); i++){
 		    	ArrayList<String> rcdRead = new ArrayList<String>();
 				br = new BufferedReader(new FileReader(args[0] + File.separator + rcdfile.get(i)));
 				String str;
-				int price = 0;
 				while((str = br.readLine()) != null){
 					rcdRead.add(str);
 				}
-
-				if(rcdRead.isEmpty()){
+				if(rcdRead.isEmpty() || rcdRead.size() / 3 != 1){
 					errorHandingDisplay(rcdfile.get(i), "illegalFormat");
 					return "illegalFormat";
 				}
-				if(rcdRead.size() % 3 != 0){
-					errorHandingDisplay(rcdfile.get(i), "illegalFormat");
-					return "illegalFormat";
-				}
-				if(rcdRead.get(2).length() < 10){
-					price = Integer.parseInt(rcdRead.get(2));
-				}else{
-					return "overflow";
-				}
-				if(beanchSalesMap.containsKey(rcdRead.get(0))){
-					beanchSalesMap.put(rcdRead.get(0), beanchSalesMap.get(rcdRead.get(0)) + price);
+				if(branchSalesMap.containsKey(rcdRead.get(0))){
+					branchSalesMap.put(rcdRead.get(0), branchSalesMap.get(rcdRead.get(0)) + Long.parseLong(rcdRead.get(2)));
 				}else{
 					errorHandingDisplay(rcdfile.get(i) + "の支店", "codeFraud");
 					return "codeFraud";
 				}
 				if(commoditySalesMap.containsKey(rcdRead.get(1))){
-					commoditySalesMap.put(rcdRead.get(1), commoditySalesMap.get(rcdRead.get(1)) + price);
+					commoditySalesMap.put(rcdRead.get(1), commoditySalesMap.get(rcdRead.get(1)) + Long.parseLong(rcdRead.get(2)));
 				}else{
 					errorHandingDisplay(rcdfile.get(i) + "の商品", "codeFraud");
 					return "codeFraud";
 				}
-				for(String key : rcdMap.keySet()){
-					if(rcdMap.get(key).toString().length() > 10){
+				for(String key : branchSalesMap.keySet()){
+					if(branchSalesMap.get(key).toString().length() > 10){
+						return "overflow";
+					}
+				}
+				for(String key : commoditySalesMap.keySet()){
+					if(commoditySalesMap.get(key).toString().length() > 10){
 						return "overflow";
 					}
 				}
@@ -131,32 +124,33 @@ public class UriageSyukei {
 	}
 
 	static void fileOutput(String[] args, String files,
-		LinkedHashMap<String, String> map, LinkedHashMap<String, Integer> salesMap) throws IOException{
+		LinkedHashMap<String, String> map, LinkedHashMap<String, Long> salesMap) throws IOException{
 		FileWriter fw = null;
 		try{
 			File file = new File(args[0] + File.separator + files);
 			fw = new FileWriter(file);
-			Map<Integer, String> out = new TreeMap<Integer, String>();
-			int a = 0;
+			Map<Long, String> out = new TreeMap<Long, String>();
+			long a = 0;
 			for(String str : map.keySet()){
 				if(salesMap.get(str) != 0){
 					out.put(salesMap.get(str), str + ',' + map.get(str));
 				}else{
 					a++;
-					out.put(a, str + ',' + map.get(str));
+					out.put(a,  str + ',' + map.get(str) + "," + salesMap.get(str));
 				}
 			}
-			List<Entry<Integer, String>> entries = new ArrayList<Entry<Integer, String>>(out.entrySet());
-			Collections.sort(entries, new Comparator<Entry<Integer, String>>(){
-				public int compare(Entry<Integer, String> o1, Entry<Integer, String> o2){
+			List<Entry<Long, String>> entries = new ArrayList<Entry<Long, String>>(out.entrySet());
+			Collections.sort(entries, new Comparator<Entry<Long, String>>(){
+				@Override
+				public int compare(Entry<Long, String> o1, Entry<Long, String> o2){
 					return o2.getKey().compareTo(o1.getKey());
 				}
 			});
-			for(Entry<Integer,String> entry : entries){
+			for(Entry<Long, String> entry : entries){
 				if(!entry.getKey().equals(a)){
 					fw.write(out.get(entry.getKey()) + "," + entry.getKey() + System.getProperty("line.separator"));
 				}else{
-					fw.write(out.get(entry.getKey()) + "," + 0 + System.getProperty("line.separator"));
+					fw.write(out.get(entry.getKey()) + System.getProperty("line.separator"));
 					a--;
 				}
 			}
@@ -166,11 +160,7 @@ public class UriageSyukei {
 			throw e;
 		}finally{
 			if(fw != null){
-				try{
-					fw.close();
-				}catch(IOException e){
-					e.printStackTrace();
-				}
+				fw.close();
 			}
 		}
 	}
@@ -201,19 +191,19 @@ public class UriageSyukei {
 		ArrayList<String> rcdfile = new ArrayList<String>();
 		LinkedHashMap<String, String> branchMap = new LinkedHashMap<String, String>();
 		LinkedHashMap<String, String> commodityMap = new LinkedHashMap<String, String>();
-		LinkedHashMap<String, Integer> beanchSalesMap = new LinkedHashMap<String, Integer>();
-		LinkedHashMap<String, Integer> commoditySalesMap = new LinkedHashMap<String, Integer>();
+		LinkedHashMap<String, Long> branchSalesMap = new LinkedHashMap<String, Long>();
+		LinkedHashMap<String, Long> commoditySalesMap = new LinkedHashMap<String, Long>();
 		LinkedHashMap<String, Integer> rcdMap = new LinkedHashMap<String, Integer>();
 
-		String str = null;
+		String errorCharacter = null;
 		boolean hasError = false;
 		try{
-			str = loadFile(args, "branch.lst", "^\\d{3}$", branchMap, beanchSalesMap);
-			if(more.equals(str)){
-				errorHandingDisplay("支店定義", str);
+			errorCharacter = loadFile(args, "branch.lst", "^\\d{3}$", branchMap, branchSalesMap);
+			if(more.equals(errorCharacter)){
+				errorHandingDisplay("支店定義", errorCharacter);
 				return;
-			}else if(match.equals(str)){
-				errorHandingDisplay("支店定義", str);
+			}else if(match.equals(errorCharacter)){
+				errorHandingDisplay("支店定義", errorCharacter);
 				return;
 			}
 		}catch(FileNotFoundException e){
@@ -223,12 +213,12 @@ public class UriageSyukei {
 			return;
 		}
 		try{
-			str = loadFile(args, "commodity.lst", "^\\w{8}$", commodityMap, commoditySalesMap);
-			if(more.equals(str)){
-				errorHandingDisplay("商品定義", str);
+			errorCharacter = loadFile(args, "commodity.lst", "^\\w{8}$", commodityMap, commoditySalesMap);
+			if(more.equals(errorCharacter)){
+				errorHandingDisplay("商品定義", errorCharacter);
 				return;
-			}else if(match.equals(str)){
-				errorHandingDisplay("商品定義", str);
+			}else if(match.equals(errorCharacter)){
+				errorHandingDisplay("商品定義", errorCharacter);
 				return;
 			}
 		}catch(FileNotFoundException e){
@@ -237,19 +227,19 @@ public class UriageSyukei {
 		}
 
 		hasError = numberCheck(args, rcdfile);
-		if(hasError == false){
+		if(!hasError){
 			errorHandingDisplay("売上", "notSequence");
 			return;
 		}
 
 		try{
-			str = aggregateData(args, beanchSalesMap, commoditySalesMap, rcdMap, rcdfile);
-			if(illegal.equals(str)){
+			errorCharacter = aggregateData(args, branchSalesMap, commoditySalesMap, rcdMap, rcdfile);
+			if(illegal.equals(errorCharacter)){
 				return;
-			}else if(over.equals(str)){
+			}else if(over.equals(errorCharacter)){
 				errorHandingDisplay("合計", "overflow");
 				return;
-			}else if(fraud.equals(str)){
+			}else if(fraud.equals(errorCharacter)){
 				return;
 			}
 		}catch(IOException e){
@@ -258,7 +248,7 @@ public class UriageSyukei {
 		}
 
 		try{
-			fileOutput(args, "branch.out",  branchMap, beanchSalesMap);
+			fileOutput(args, "branch.out",  branchMap, branchSalesMap);
 		}catch(IOException e){
 			return;
 		}
